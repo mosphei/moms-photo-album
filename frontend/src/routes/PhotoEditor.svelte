7945865b191a5d25794188c561cd419f6535c0ba
jarom <jb@packetsmiter.com>
2025-12-03 09:14:28 -0800

    UI

--- frontend/src/routes/PhotoEditor.svelte
+++ frontend/src/routes/PhotoEditor.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Photo } from '$lib/models/photo';
+	import { photoPath, type Photo } from '$lib/models/photo';
 	import Thumbnail from './Thumbnail.svelte';
 
 	interface IProps {
@@ -17,12 +17,127 @@
 		if (onsave) onsave();
 		throw new Error('Function not implemented.');
 	}
+	// get initial values
+	const originalValues = {
+		date_taken: photos
+			.map((p) => (p.date_taken ? p.date_taken.toLocaleDateString() : null))
+			.filter((v) => v)
+			.join(','),
+		description: photos
+			.map((p) => (p.description ? `${p.description.substring(0, 10)}...` : null))
+			.filter((v) => v)
+			.join(','),
+		people: photos.flatMap((p) => p.people).join(',')
+	};
+	let fieldsToUpdate: string[] = $state([]);
+	let date_taken = $state('');
+	let description = $state('');
+	let rotate = $state(0);
 </script>
 
-<div class="row g-1 mb-2">
+<div class="d-flex flex-wrap mb-2" style="width: 100%;--rotation:{rotate}deg;">
 	{#each photos as photo}
-		<div class="col-auto"><Thumbnail {photo} /></div>
+		<div style="margin:4px; width: 200px; height:200px">
+			<img
+				src={photoPath('t', photo)}
+				alt={photo.filename}
+				style="object-fit: contain;width:100%;height:100%;transform:rotate(var(--rotation,0));transform-origin:center center"
+			/>
+		</div>
 	{/each}
+</div>
+
+<div class="mb-3">
+	<p>Unchecked attributes will not be changed on the target photos.</p>
+	<div class="mb-2">
+		<label for="date">Date</label>
+		<div class="input-group">
+			<div class="input-group-text">
+				<input
+					type="checkbox"
+					value="date_taken"
+					bind:group={fieldsToUpdate}
+					class="form-check-input"
+				/>
+			</div>
+			<input
+				type={fieldsToUpdate.includes('date_taken') ? 'date' : 'text'}
+				class="form-control"
+				bind:value={date_taken}
+				placeholder={originalValues.date_taken}
+				name="date"
+				id="date"
+				disabled={!fieldsToUpdate.includes('date_taken')}
+			/>
+		</div>
+	</div>
+	<div class="mb-2">
+		<label for="description">Description</label>
+		<div class="input-group">
+			<div class="input-group-text">
+				<input
+					type="checkbox"
+					value="description"
+					bind:group={fieldsToUpdate}
+					class="form-check-input"
+				/>
+			</div>
+
+			<input
+				type="text"
+				class="form-control"
+				bind:value={description}
+				name="description"
+				id="description"
+				disabled={!fieldsToUpdate.includes('description')}
+			/>
+		</div>
+	</div>
+	<div class="mb-2">
+		<label for="people">People</label>
+		<div class="input-group">
+			<div class="input-group-text">
+				<input
+					type="checkbox"
+					value="people"
+					bind:group={fieldsToUpdate}
+					class="form-check-input"
+				/>
+			</div>
+			<input
+				type="text"
+				class="form-control"
+				name="description"
+				id="description"
+				disabled={!fieldsToUpdate.includes('people')}
+			/>
+		</div>
+	</div>
+	<div class="mb-2">
+		<label for="date">Rotate</label>
+		<div class="input-group">
+			<div class="input-group-text">
+				<input
+					type="checkbox"
+					value="rotate"
+					bind:group={fieldsToUpdate}
+					class="form-check-input"
+				/>
+			</div>
+			<select
+				name="rotate"
+				id="rotate"
+				class="form-select"
+				bind:value={rotate}
+				disabled={!fieldsToUpdate.includes('rotate')}
+			>
+				<option value={0}>Rotate:</option>
+				<option value={270}>left</option>
+				<option value={90}>right</option>
+				<option value={180}>180</option>
+			</select>
+		</div>
+	</div>
 </div>
 <div class="d-flex justify-content-between">
 	<button class="btn btn-primary" onclick={save}>Save</button>
